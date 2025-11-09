4ea91dfdcc9f51f3f85facdf6b762ef33afb6b22
cjag99 <cjag99@gmail.com>
2025-11-09 11:14:40 +0100

    Update JS functions exercise

--- Functions/main.js
+++ Functions/main.js
@@ -38,3 +38,87 @@ function ej2JS() {
   let parrafo = document.getElementById("act2");
   parrafo.innerHTML += `<ul>${lista}</ul>`;
 }
+function recogerParams(id) {
+  let cadena = document.getElementById(id).value;
+  let cadenaArray = cadena.split(",").map((e) => e.trim());
+  return cadenaArray;
+}
+function listaEn(id, ...cadena) {
+  let parrafo = document.getElementById(id);
+  let items = cadena.map((e) => `<li>${e}</li>`).join("");
+  parrafo.innerHTML = `<ul>${items}</ul>`;
+}
+function mostrarTriple() {
+  let numParam = parseFloat(document.getElementById("tripleNum").value);
+  let parrafo = document.getElementById("act4");
+  let tripleNum = function (n) {
+    return n * 3;
+  };
+  parrafo.innerHTML = `El triple de ${numParam} es ${tripleNum(numParam)}`;
+}
+
+function mostrarCuadrado() {
+  let numParam = parseFloat(document.getElementById("cuadradoNum").value);
+  let parrafo = document.getElementById("act5");
+  let cuadradoNum = (n) => n ** 2;
+  parrafo.innerHTML = `El cuadrado de ${numParam} es ${cuadradoNum(numParam)}`;
+}
+
+// * Función anonima hola mundo con ejecución automática
+(() => (document.getElementById("act6").innerHTML = "Hola Mundo"))();
+
+function mostrarListaNegrita(id, ...cadena) {
+  let parrafo = document.getElementById(id);
+  let items = cadena.map((e) => `<strong>${e}</strong>, `).join("");
+  parrafo.innerHTML =
+    "Los valores del array en negrita son: " + items.valueOf();
+}
+function generarTabla() {
+  let parrafo = document.getElementById("act8");
+  parrafo.innerHTML =
+    "<table border='1'>" +
+    [
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ]
+      .map(
+        (row) =>
+          `<tr>${row
+            .map((cell) => `<td>${Math.round(9 * Math.random())}</td>`)
+            .join("")}</tr>`
+      )
+      .join("") +
+    "</table>";
+}
+function generarCuadradoMagico() {
+  let i = parseInt(document.getElementById("magicSquare").value);
+  let parrafo = document.getElementById("act9");
+  parrafo.innerHTML =
+    "<table border='1'>" +
+    [
+      [i + 3, i + 8, i + 1],
+      [i + 2, i + 4, i + 6],
+      [i + 7, i, i + 5],
+    ]
+      .map(
+        (row) => `<tr>${row.map((cell) => `<td>${cell}</td>`).join("")}</tr>`
+      )
+      .join("") +
+    "</table>";
+}
+function cambiarColor() {
+  let nombreColor = document.getElementById("nombreColor").value;
+  let texto = document.getElementById("act10");
+  texto.style.color = nombreColor;
+}
+function factorial(num) {
+  return num === 1 ? 1 : num * factorial(num - 1);
+}
+function mostrarFactorial() {
+  let parrafo = document.getElementById("act11");
+  let num = parseInt(document.getElementById("factorial").value);
+  let numFactorial = factorial(num);
+  parrafo.innerHTML = `El factorial de ${num} es ${numFactorial}`;
+}
